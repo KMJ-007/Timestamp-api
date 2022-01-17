2ce9c7d3d110a58c0d3967c70bbd00f597133fa7
Karan Janthe <karanjanthe@gmail.com>
2022-01-17 15:36:33 +0530

    completed the project

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*
+question breakdown=
+
+so the user will can will give us input and using that input we have to give him timestamp
+
+so let's think which type of input can user give us 
+1.2022-01-17
+2.unix timestamp means an integer
+3.nothing means blank input
+ */
+
 // server.js
 // where your node app starts
 
@@ -19,14 +30,55 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+// // your first API endpoint... 
+// app.get("/api/hello", function (req, res) {
+//   res.json({greeting: 'hello API'});
+// });
 
+//api endpoint for timestamp
+//the ? is to take input after api/ not to take as route
+app.get("/api/:dateInput?", function(req,res){
+  //let's store the input in some const
+  const dateInput=req.params.dateInput;
+  let date;
+  // console.log(!dateInput);
+  //if user enter's empty input then output should today's date
+  if(!dateInput){
+    date = new Date(); 
+  }
+  else{
+    //simple regx that checks yyyy-mm-dd
+    let dateFormate= /\d{4}-\d{2}-\d{2}/.test(dateInput);
+    // console.log(dateFormate);
+    // console.log(typeof(dateInput));
+    if(dateFormate){
+      //the input will e string we have to converted it to int
+      date = new Date(parseInt(dateInput));
+    }
+    else{
+      // console.log(dateInput);
+      date = new Date(dateInput);
+    }
 
+    
+  }
+  // console.log(date);
+
+  // sending the json response 
+  if(date.toUTCString() === "Invalid Date"){
+    res.json({
+      'error':date.toUTCString()
+    })
+  }
+  else{
+  res.json({
+    'unix':date.getTime(),
+    'utc':date.toUTCString() 
+  })
+}
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT||3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
